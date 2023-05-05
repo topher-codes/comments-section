4c6b1dadf36805c41ca94c9e2edaee2299fa6b0c
Chris Andrews <andrewsc33@icloud.com>
2023-05-04 20:28:23 -0700

    fix
    destructured the commentz
    
    cause fix

--- web/src/components/Comment.tsx
+++ web/src/components/Comment.tsx
@@ -1,8 +1,7 @@
 import clsx from 'clsx';
+import Image from 'next/image';
 
-interface CommentProps {
-  className?: string;
-  children?: React.ReactNode;
+interface Comment {
   body: string;
   authorName: string;
   authorAvatar: string;
@@ -10,14 +9,22 @@ interface CommentProps {
   rating: number;
 }
 
-const Comment = ({ className, children, body, authorName, authorAvatar, createdAt }: CommentProps) => { 
-  const rootClassName = clsx(className, 'flex flex-col space-y-2');
+interface CommentProps {
+  className?: string;
+  children?: React.ReactNode;
+  comment: Comment;
+}
+
+const Comment = ({ className, children, comment }: CommentProps) => { 
+  const rootClassName = clsx(className, 'flex flex-col space-y-2 p-2 border border-slate-800 rounded-md');
+
+  const { body, authorName, authorAvatar, createdAt, rating } = comment;
 
   return (
     <div className={rootClassName}>
       <div className="flex space-x-2">
         <div className="w-8 h-8 rounded-full overflow-hidden">
-          <img src={authorAvatar} alt={authorName} />
+         <Image src={authorAvatar} alt={authorName} width={32} height={32} /> 
         </div>
         <div className="flex flex-col space-y-1">
           <div className="flex items-center space-x-1">
@@ -25,6 +32,7 @@ const Comment = ({ className, children, body, authorName, authorAvatar, createdA
             <span className="text-xs text-gray-500">{createdAt}</span>
           </div>
           <p className="text-sm">{body}</p>
+          <p>{rating}</p>
         </div>
       </div>
       {children}
