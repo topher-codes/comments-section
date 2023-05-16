54323f159e01983a7c2cb5db6e94cfeb894915b9
Chris Andrews <andrewsc33@icloud.com>
2023-05-16 16:30:13 -0700

    potential build fix

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,7 @@ await import("./src/env.mjs");
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
+  experimental: { appDir: false },
   images: {
     domains: ["cdn.discordapp.com", "lh3.googleusercontent.com"],
   },
